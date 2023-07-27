bbf12768c8b9f2704f577a69e45bf86e48862582
EngAhmadHawa <bassibassil850@gmail.com>
2023-07-27 18:40:14 +0300

    add the project

---
+++ try-face/attendance.py
@@ -0,0 +1,311 @@
+import tkinter as tk
+from tkinter import *
+import os, cv2
+import shutil
+import csv
+import numpy as np
+from PIL import ImageTk, Image
+import pandas as pd
+import datetime
+import time
+import tkinter.font as font
+import pyttsx3
+
+# project module
+import show_attendance
+import takeImage
+import trainImage
+import automaticAttedance
+
+# engine = pyttsx3.init()
+# engine.say("Welcome!")
+# engine.say("Please browse through your options..")
+# engine.runAndWait()
+
+
+def text_to_speech(user_text):
+    engine = pyttsx3.init()
+    engine.say(user_text)
+    engine.runAndWait()
+
+
+haarcasecade_path = (r"C:\Users\Ahmad\PycharmProjects\ahmad\try-face\haarcascade_frontalface_default.xml")
+trainimagelabel_path = (
+    r"C:\Users\Ahmad\PycharmProjects\ahmad\try-face\TrainingImageLabel\Trainner.yml"
+)
+trainimage_path = r"C:\Users\Ahmad\PycharmProjects\ahmad\try-face\TrainingImage"
+studentdetail_path = (
+    r"C:\Users\Ahmad\PycharmProjects\ahmad\try-face\StudentDetails\studentdetails.csv"
+)
+attendance_path = r"C:\Users\Ahmad\PycharmProjects\ahmad\try-face\attendance"
+
+
+window = Tk()
+window.title("Face recognizer")
+window.geometry("1280x720")
+dialog_title = "QUIT"
+dialog_text = "Are you sure want to close?"
+window.configure(background="#6C8784")
+
+
+# to destroy screen
+def del_sc1():
+    sc1.destroy()
+
+
+# error message for name and no
+def err_screen():
+    global sc1
+    sc1 = tk.Tk()
+    sc1.geometry("400x110")
+    sc1.iconbitmap("AMS.ico")
+    sc1.title("Warning!!")
+    sc1.configure(background="black")
+    sc1.resizable(0, 0)
+    tk.Label(
+        sc1,
+        text="Enrollment & Name required!!!",
+        fg="yellow",
+        bg="black",
+        font=("times", 20, " bold "),
+    ).pack()
+    tk.Button(
+        sc1,
+        text="OK",
+        command=del_sc1,
+        fg="yellow",
+        bg="black",
+        width=9,
+        height=1,
+        activebackground="Red",
+        font=("times", 20, " bold "),
+    ).place(x=110, y=50)
+
+
+def testVal(inStr, acttyp):
+    if acttyp == "1":  # insert
+        if not inStr.isdigit():
+            return False
+    return True
+
+
+logo = Image.open("UI_Image/0001.png")
+logo = logo.resize((50, 47), Image.ANTIALIAS)
+logo1 = ImageTk.PhotoImage(logo)
+l1 = tk.Label(window, image=logo1, bg="#25373D",)
+l1.place(x=470, y=10)
+
+
+
+a = tk.Label(
+    window,
+    text="Welcome to the Face Recognition Based\nAttendance Management System",
+    bg="#6C8784",
+    fg="white",
+    font=("arial", 35),
+)
+a.pack()
+
+ri = Image.open("UI_Image/register.png")
+r = ImageTk.PhotoImage(ri)
+label1 = Label(window, image=r)
+label1.image = r
+label1.place(x=300, y=270)
+
+
+
+vi = Image.open("UI_Image/verifyy.png")
+v = ImageTk.PhotoImage(vi)
+label3 = Label(window, image=v)
+label3.image = v
+label3.place(x=1000, y=270)
+
+
+def TakeImageUI():
+    ImageUI = Tk()
+    ImageUI.title("Take Student Image..")
+    ImageUI.geometry("780x480")
+    ImageUI.configure(background="#25373D")
+    ImageUI.resizable(0, 0)
+
+    # heading
+    a = tk.Label(
+        ImageUI,
+        text="Enter the details",
+        bg="#6C8784",
+        fg="white",
+        font=("arial", 24),
+    )
+    a.place(x=280, y=75)
+
+    # ER no
+    lbl1 = tk.Label(
+        ImageUI,
+        text="Enrollment No",
+        width=10,
+        height=2,
+        bg="#6C8784",
+        fg="white",
+        font=("times new roman", 12),
+    )
+    lbl1.place(x=120, y=130)
+    txt1 = tk.Entry(
+        ImageUI,
+        width=17,
+        validate="key",
+        bg="white",
+        fg="black",
+        relief=RIDGE,
+        font=("times", 25, "bold"),
+    )
+    txt1.place(x=250, y=130)
+    txt1["validatecommand"] = (txt1.register(testVal), "%P", "%d")
+
+    # name
+    lbl2 = tk.Label(
+        ImageUI,
+        text="Name",
+        width=10,
+        height=2,
+        bg="#6C8784",
+        fg="white",
+        bd=5,
+        relief=RIDGE,
+        font=("times new roman", 12),
+    )
+    lbl2.place(x=120, y=200)
+    txt2 = tk.Entry(
+        ImageUI,
+        width=17,
+        bd=5,
+        bg="white",
+        fg="black",
+        relief=RIDGE,
+        font=("times", 25, "bold"),
+    )
+    txt2.place(x=250, y=200)
+
+    lbl3 = tk.Label(
+        ImageUI,
+        text="Notification",
+        width=10,
+        height=2,
+        bg="#6C8784",
+        fg="white",
+        bd=5,
+        relief=RIDGE,
+        font=("times new roman", 12),
+    )
+    lbl3.place(x=120, y=270)
+
+    message = tk.Label(
+        ImageUI,
+        text="",
+        width=32,
+        height=2,
+        bg="white",
+        fg="black",
+        relief=RIDGE,
+        font=("times", 12, "bold"),
+    )
+    message.place(x=250, y=270)
+
+    def take_image():
+        l1 = txt1.get()
+        l2 = txt2.get()
+        takeImage.TakeImage(
+            l1,
+            l2,
+            haarcasecade_path,
+            trainimage_path,
+            message,
+            err_screen,
+            text_to_speech,
+        )
+        txt1.delete(0, "end")
+        txt2.delete(0, "end")
+
+    # take Image button
+    # image
+    takeImg = tk.Button(
+        ImageUI,
+        text="Take Image",
+        command=take_image,
+        font=("times new roman", 18),
+        bg="#6C8784",
+        fg="white",
+        height=2,
+        width=12,
+        relief=RIDGE,
+    )
+    takeImg.place(x=130, y=350)
+
+    def train_image():
+        trainImage.TrainImage(
+            haarcasecade_path,
+            trainimage_path,
+            trainimagelabel_path,
+            message,
+            text_to_speech,
+        )
+
+    # train Image function call
+    trainImg = tk.Button(
+        ImageUI,
+        text="Train Image",
+        command=train_image,
+        font=("times new roman", 18),
+        bg="#6C8784",
+        fg="white",
+        height=2,
+        width=12,
+        relief=RIDGE,
+    )
+    trainImg.place(x=360, y=350)
+
+
+r = tk.Button(
+    window,
+    text="Register a new student",
+    command=TakeImageUI,
+    font=("times new roman", 16),
+    bg="#F29B34",
+    fg="white",
+    height=2,
+    width=17,
+)
+r.place(x=300, y=520)
+
+
+def automatic_attedance():
+    automaticAttedance.subjectChoose(text_to_speech)
+
+
+r = tk.Button(
+    window,
+    text="Take Attendance",
+    command=automatic_attedance,
+    font=("times new roman", 16),
+    bg="#F29B34",
+    fg="white",
+    height=2,
+    width=17,
+)
+r.place(x=1000, y=520)
+
+
+
+
+r = tk.Button(
+    window,
+    text="EXIT",
+    command=quit,
+    font=("times new roman", 16),
+    bg="#F29B34",
+    fg="white",
+    height=2,
+    width=17,
+)
+r.place(x=650, y=660)
+
+window.mainloop()
